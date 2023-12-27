32ac87d16b3e356f7cfa6ff793a048dad5917a95
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2023-12-27 13:08:18 +0530

    commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 import Sidebar from '../Sidebar/index.js';
+import Proj1 from "./proj1/proj1.PNG";
+import Proj2 from "./proj2/proj2.PNG";
 
 const Projects = () => {
     return(
@@ -7,8 +9,15 @@ const Projects = () => {
         <Sidebar/>
 
         <div className='projects'>
-            <div className='proj1'>
-                
+         <div className='proj1'>
+            <img src={Proj1}/>
+            <h1>Bloggy</h1>
+            <p>A fullstack blogging applications with all of the CRUD operation and authentication</p>
+            </div>   
+            <div className='proj2'>
+                <img src={Proj2} />
+                <h1>Todotwist</h1>
+                <p>A task manager application which helps you track all your todos with ease.</p>
             </div>
         </div>
         </>
