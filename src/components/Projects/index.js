c26c10e36f14e112b05cfc27d4d337d2f5f412c8
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2024-05-27 05:37:42 +0530

    new project added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,8 @@ import './index.scss';
 import Sidebar from '../Sidebar/index.js';
 import Proj1 from "./proj1/proj1.PNG";
 import Proj2 from "./proj2/proj2.PNG";
-import Proj3 from "./proj3/proj3.png"
+import Proj3 from "./proj3/proj3.png";
+import Proj4 from "./proj4/proj4.JPG";
 
 
 const Projects = () => {
@@ -16,6 +17,11 @@ const Projects = () => {
                 <img src={Proj3} alt='project3'/>
                 <h1>BestGo Wallet</h1>
                 <p>A demo digital wallet that gives you a random balance and lets you transfer it.</p>
+            </div>
+        <div className='proj4' onClick={()=>window.location.href="https://bestgowallet-frontend-51jr8hdna-aravindtm27s-projects.vercel.app/"}>
+                <img src={Proj4} alt='project4'/>
+                <h1>Chatbot Flow Builder</h1>
+                <p>A flow builder project using reactflow library.</p>
             </div>
          <div className='proj1'>
             <img src={Proj1} alt='project1'/>
