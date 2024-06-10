c3f4182df1c0c56af0c17d570159d40f65ca0e3e
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2024-06-11 04:55:28 +0530

    new project added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import Sidebar from '../Sidebar/index.js';
-import Proj1 from "./proj1/proj1.PNG";
+import Proj1 from "./proj1/proj1.JPG";
 import Proj2 from "./proj2/proj2.PNG";
 import Proj3 from "./proj3/proj3.png";
 import Proj4 from "./proj4/proj4.JPG";
@@ -25,8 +25,8 @@ const Projects = () => {
             </div>
          <div className='proj1'>
             <img src={Proj1} alt='project1'/>
-            <h1>Bloggy</h1>
-            <p>A fullstack blogging applications with all of the CRUD operation and authentication</p>
+            <h1>Medium Clone</h1>
+            <p>A fullstack blogging applications with all of the CRUD operation and authentication, using react and backend deployed on Cloudflare</p>
             </div>   
             <div className='proj2'>
                 <img src={Proj2} alt='project2'/>
